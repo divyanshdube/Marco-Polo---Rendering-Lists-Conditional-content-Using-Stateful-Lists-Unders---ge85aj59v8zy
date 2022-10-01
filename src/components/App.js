e67619b8d26dc36c725b79790f61d6dd98591935
Divyansh Dubey <108198974+divyanshdube@users.noreply.github.com>
2022-10-02 01:02:59 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,24 @@
-import React from 'react'
-import '../styles/App.css';
+import React from "react";
+import "../styles/App.css";
+
 const App = () => {
-  
+  isToggleMarco = true;
+
+  textHandler = () => {
+    setState((state) => {
+      isToggleMarco = !state.isToggleMarco;
+    });
+  };
+
   return (
     <div id="main">
-
+      <h1 id="marco-polo">Marco</h1>
+      <button id="marco-polo-toggler" onClick={textHandler}>
+        Polo
+      </button>
+      {state.isToggleMarco ? "Polo" : "Marco"}
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
